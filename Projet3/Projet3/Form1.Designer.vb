710a9949261e674d4a26ee5bfcb075944fa87a90
RootAba <Abassemaiga403@gmail.com>
2022-10-17 11:51:44 +0200

    first com for pro3 vba

---
+++ Projet3/Projet3/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.RadioButton()
+        Me.btn2 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.AutoSize = True
+        Me.btn1.Location = New System.Drawing.Point(23, 31)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(92, 19)
+        Me.btn1.TabIndex = 0
+        Me.btn1.TabStop = True
+        Me.btn1.Text = "6 p.m _8 a.m"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.AutoSize = True
+        Me.btn2.Location = New System.Drawing.Point(23, 67)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(95, 19)
+        Me.btn2.TabIndex = 1
+        Me.btn2.TabStop = True
+        Me.btn2.Text = "8 a.m - 6 p.m"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btn1)
+        Me.GroupBox1.Controls.Add(Me.btn2)
+        Me.GroupBox1.Location = New System.Drawing.Point(307, 99)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(613, 114)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI Semibold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(499, 205)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(126, 38)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Envoyer"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(544, 116)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 15)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "La Durée"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(483, 271)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(221, 21)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Le cout totale de l'appel est :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(233, 258)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(92, 21)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Coût brut : "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn1 As RadioButton
+    Friend WithEvents btn2 As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Projet3/Projet3/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        Dim temps As Integer = CInt(TextBox1.Text)
+        ' 20 cents par minute et une taxe de 15%
+        Dim coutT As Double = (temps * 0.2) + ((15 * (temps * 0.2)) / 100)
+        Dim coutTotal As Double
+
+        Label3.Text = "Coût brut :" & coutT & "$"
+
+        'Si de 8p.m à 6 a.m
+        If (btn1.Checked) Then
+
+            coutT -= (50 * coutT) / 100
+
+            ' temps superieur à 60 mn
+            If (temps > 60) Then
+                coutTotal = coutT - ((20 * coutT) / 100)
+
+            Else
+                coutTotal = coutT
+
+            End If
+
+            Label2.Text = "Le cout totale de l'appel est :" & coutTotal & "$"
+
+            'si 6 a.m à 8 p.m
+        ElseIf (btn2.Checked) Then
+            If (temps > 60) Then
+                coutTotal = coutT - ((20 * coutT) / 100)
+
+            Else
+                coutTotal = coutT
+
+            End If
+
+            Label2.Text = "Le cout totale de l'appel est :" & coutTotal & "$"
+
+        End If
+
+    End Sub
+
+
+End Class
